05610a7fd1aa6821ab77a6c18229103bca87e2ea
jmcmanusx47 <jamesmcmanusx47@gmail.com>
2022-02-28 00:31:07 -0500

    player cant move after dying
    added an isDead bool to playercontroller, can move if isDead is false, isDead is only set to true when the player dies in PlayerHealth.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,33 +6,47 @@ public class PlayerController : MonoBehaviour {
     public float moveDelay = .1f;
     private float timePassed;
     private Vector2 movement;
+    [HideInInspector] public bool isDead = false;
 
     // Start is called before the first frame update
     void Start() {
         timePassed = moveDelay;
+        isDead = false;
     }
 
     // Update is called once per frame
     void Update() {
         timePassed += Time.deltaTime;
 
-        if (timePassed >= moveDelay) {
-            movement.x = Input.GetAxisRaw("Horizontal");
-            movement.y = Input.GetAxisRaw("Vertical");
+        if (!isDead) {
+            if (timePassed >= moveDelay)
+            {
+                movement.x = Input.GetAxisRaw("Horizontal");
+                movement.y = Input.GetAxisRaw("Vertical");
 
-            if (movement.x != 0 || movement.y != 0) {
-                timePassed = 0;
-            }
+                if (movement.x != 0 || movement.y != 0)
+                {
+                    timePassed = 0;
+                }
 
-            if (movement.x > 0) {
-                transform.position = transform.position + new Vector3(1, 0);
-            } else if (movement.x < 0) {
-                transform.position = transform.position - new Vector3(1, 0);
-            } else if (movement.y > 0) {
-                transform.position = transform.position + new Vector3(0, 0, 1);
-            } else if (movement.y < 0) {
-                transform.position = transform.position - new Vector3(0, 0, 1);
+                if (movement.x > 0)
+                {
+                    transform.position = transform.position + new Vector3(1, 0);
+                }
+                else if (movement.x < 0)
+                {
+                    transform.position = transform.position - new Vector3(1, 0);
+                }
+                else if (movement.y > 0)
+                {
+                    transform.position = transform.position + new Vector3(0, 0, 1);
+                }
+                else if (movement.y < 0)
+                {
+                    transform.position = transform.position - new Vector3(0, 0, 1);
+                }
             }
         }
+        
     }
 }
