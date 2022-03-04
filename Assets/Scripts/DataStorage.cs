f974f29f2738dd7f4a9f8d95ec8354d85ee35c0b
Justin Lewis <jlewisait@gmail.com>
2022-03-04 16:58:23 -0500

    Prefabs plus static healthbar

---
+++ Assets/Scripts/DataStorage.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataStorage : MonoBehaviour
+{
+    public static float currentHealth = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static void TakeDamage(float damage) {
+        currentHealth -= damage;
+    }
+}
